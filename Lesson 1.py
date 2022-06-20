58cbda480cbb695fe9ced8b304291b9ca0e1ee83
CrazyMegaMishun <62602625+CrazyMegaMishun@users.noreply.github.com>
2022-06-20 19:22:55 +0300

    refactored lesson 1

--- Lesson 1.py
+++ Lesson 1.py
@@ -1,10 +1,13 @@
+#import this - виводить Zen of Python
+#import antigravity - відкриває браузер з коміксом про пітон))
+#import __hello__ - виводить "Hello world!"
+#from __future__ import braces - має імпортувати функції з наступних версій пітона в попередні,
+
 from art import *
-tprint("Misha", font="caligraphy")
+tprint("Misha", font="calligraphy")
 
-x = 66
-y = 5
-
-print('x =', x, 'y =', y)
+x = int(input('x = '))
+y = int(input('y = '))
 print(x + y)
 print(x // y)
 print(x % y)
