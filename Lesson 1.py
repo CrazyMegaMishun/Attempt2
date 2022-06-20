c0d61ab881d6024f84aa0a0257ab8d0081378e6d
CrazyMegaMishun <62602625+CrazyMegaMishun@users.noreply.github.com>
2022-06-20 19:14:59 +0300

    refactored lesson 1

--- Lesson 1.py
+++ Lesson 1.py
@@ -1,3 +1,8 @@
+#import this - виводить Zen of Python
+#import antigravity - відкриває браузер з коміксом про пітон))
+#import __hello__ - виводить "Hello world!"
+#from __future__ import braces - має імпортувати функції з наступних версій пітона в попередні,
+
 from art import *
 tprint("Misha", font="calligraphy")
 
