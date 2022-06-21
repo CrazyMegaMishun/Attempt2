24b99d82e025adcbdc805dd765325fbd03a6d6fb
CrazyMegaMishun <62602625+CrazyMegaMishun@users.noreply.github.com>
2022-06-21 21:32:16 +0300

    Update Lesson 3.py
    Almost ready hometask Lesson3

---
+++ Lesson 3.py
@@ -0,0 +1,42 @@
+"""#Завдання 1
+print(sum(map(int, list(input()))))
+
+#Завдання 2
+x = str(float(input()))
+print(x[x.find('.')+1:])
+print((x[x.find('.')+1:])[0])
+
+#Завдання 3
+list_ten = [10, 20, 30, 40, 50]
+for i in list_ten[::-1]:
+    print(i, end=' ')
+
+#Завдання 4
+list_of_six = [100, 106, 112, 118, 124, 130, 136, 142, 148, 154, 160, 166, 172, 178, 184, 190, 196]
+for i in list_of_six:
+    if i % 5 == 0 and i < 150:
+        print(i, end=' ')
+
+#Завдання 5
+import random
+for i in range(3):
+    x = int(input())
+    if x == random.randint(1, 10):
+        print('You won!')
+        break
+    else:
+        print('You lose!')
+
+#Завдання 6
+horizontal = 'ABCDEFGH'
+
+cell_1 = input('Please write down first cell:')
+cell_2 = input('Please write down second cell:')
+
+if horizontal.find(cell_1[0]) - horizontal.find(cell_2[0]) == 1 or horizontal.find(cell_1[0]) - horizontal.find(cell_2[0]) == -1:
+    if int((cell_1[1])) - int(cell_2[1]) == 2 or int(cell_1[1]) - int(cell_2[1]) == -2:
+        print(True)
+    else:
+        print(False)
+"""
+#Завдання 7
