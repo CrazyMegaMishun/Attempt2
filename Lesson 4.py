c3a7f89749d821aa9feb0d5e71ea4e0b1fb0394b
CrazyMegaMishun <62602625+CrazyMegaMishun@users.noreply.github.com>
2022-06-24 01:44:51 +0300

    Create Lesson 4.py

---
+++ Lesson 4.py
@@ -0,0 +1,47 @@
+"""# Завдання 6
+x = int(input())
+
+
+def highest_neighbor_number(x):
+    y = list()
+    b = 0
+    while x > 0:
+        y.append(x % 10)
+        x = (x - (x % 10)) // 10
+
+    for i in range(len(y)-1):
+        if y[i-1] < y[i] > y[i+1]:
+            b += 1
+    return b
+
+
+print(highest_neighbor_number(x))
+"""
+#Завдання 7
+list_1 = int(input())
+list_2 = int(input())
+
+
+def list_maker(x):
+    y = list()
+    while x > 0:
+        y.append(x % 10)
+        x = (x - (x % 10)) // 10
+    return y
+
+
+def search_common(): #общие для двух
+    pass
+
+
+def unique_first(): #есть в первом, но нет во втором
+    pass
+
+
+def unique_both(): #уникальные для обоих
+    pass
+
+
+print(search_common())
+print(unique_first())
+print(unique_both())
