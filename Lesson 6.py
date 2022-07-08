785e4cb76de8225b9170fec5a2053aad493e0b8d
CrazyMegaMishun <62602625+CrazyMegaMishun@users.noreply.github.com>
2022-07-08 23:22:14 +0300

    work in progress

---
+++ Lesson 6.py
@@ -0,0 +1,19 @@
+"""#Завдання 1
+coin = ('Bitcoin', 'Ether', 'Ripple', 'Litecoin')
+code = ('BTC', 'ETH', 'XRP', 'LTC')
+
+
+def dict_maker(keys, values):
+    x = {}
+
+    for i in range(len(keys)):
+        x[keys[i]] = values[i]
+
+    return x
+
+
+print(dict_maker(coin, code))
+"""
+
+#Завдання 2
+
