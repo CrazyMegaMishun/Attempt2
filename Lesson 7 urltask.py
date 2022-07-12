92a6c39e5db8cab76653245e8974b7f99058f65a
CrazyMegaMishun <62602625+CrazyMegaMishun@users.noreply.github.com>
2022-07-13 01:28:22 +0300

    l7

--- Lesson 7 urltask.py
+++ Lesson 7 urltask.py
@@ -1,16 +1,38 @@
+import datetime
 import json
+import time
 import requests
-from urllib import request
-from http.client import HTTPResponse
 
 
-def get_site():
-    return request.urlopen('http://api.openweathermap.org/data/2.5/forecast/daily?q=city&cnt=1&units=metric&appid=f9ada9efec6a3934dad5f30068fdcbb8')
+def get_site_by_requests() -> dict:
+    q = input('Введіть місто: ')
+    cnt = input('Введіть кількість днів: ')
+
+    link = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + q + '&' + 'cnt=' + cnt + '&units=metric&appid=f9ada9efec6a3934dad5f30068fdcbb8'
+
+    return requests.get(link).json()
 
 
-def get_site_by_requests() -> HTTPResponse:
-    return requests.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=city&cnt=1&units=metric&appid=f9ada9efec6a3934dad5f30068fdcbb8', params='city')
+def site_to_json(j) -> json:
+    x = (j["list"])
+
+    with open('temperature.json', 'w') as file:
+        json.dump(x, file, indent=2)
 
 
-print(get_site_by_requests())
+def json_decrypt(j_file):
+    with open(j_file, 'r+') as f:
+        for i in enumerate(json.load(f)):
+            date_str = str(datetime.datetime.fromtimestamp(i[1]['dt'])).split()[0]
+            print(j_file)
+
+
+
+        """for i in enumerate(json.load(f)):
+            print(str(datetime.datetime.fromtimestamp("%y-%m-%d", i[1]['dt'])) + ' ' + str(i[1]['temp']['min']) + ' ' + str(i[1]['temp']['max']))"""
+
+
+site_to_json(get_site_by_requests())
+json_decrypt('temperature.json')
+
 
