3c7c1d1375323d6785437ae2ea7a4f15cfe63c61
CrazyMegaMishun <62602625+CrazyMegaMishun@users.noreply.github.com>
2022-07-18 21:39:25 +0300

    l8

--- exchange_rates.py
+++ exchange_rates.py
@@ -2,17 +2,18 @@ import argparse
 import json
 from datetime import datetime, timedelta, date
 import requests
+from tabulate import tabulate
 
 parser = argparse.ArgumentParser(description='Онлайн конвертер валют')
-parser.add_argument('-cf', '--currency_from', type=str, default='USD', help='Що конвертуємо')
-parser.add_argument('-ct', '--currency_to', type=str, default='UAH', help='В яку валюту конвертуємо')
-parser.add_argument('-a', '--amount', type=float, default=100.00, help='Кількість')
-parser.add_argument('-sd', '--start_date', type=str, default=str(date.today()), help='Дата')
-parser.add_argument('-save', '--save_to_file', type=str, help='Зберігаємо у файл?')
+parser.add_argument('cf', type=str, default='USD', help='Що конвертуємо')
+parser.add_argument('ct', type=str, default='UAH', help='В яку валюту конвертуємо')
+parser.add_argument('a', type=float, default=100.00, help='Кількість')
+parser.add_argument('-sd', '--start_date', type=str, default=str(date.today()), help='Дата (рік-місяць-день)')
+parser.add_argument('-save', '--save_to_file', type=str, help='Зберігаємо у файл (Y/N)?')
 args, unknown = parser.parse_known_args()
 
 
-def date_check(d) -> date:
+def date_check(d) -> str:
     try:
         if datetime.strptime(d, "%Y-%m-%d") - timedelta(days=1) >= datetime.today():
             return str(date.today()).split()[0]
@@ -30,12 +31,12 @@ def currency_check(currency) -> str:
             return ''
 
 
-def get_site_by_requests(i):
+def get_site_by_requests(i) -> json:
     link = 'https://api.exchangerate.host/convert?'
-    c1 = 'from=' + currency_check(args.currency_from) + '&'
-    c2 = 'to=' + currency_check(args.currency_to) + '&'
-    number = 'amount=' + str(args.amount) + '&'
-    d1 = 'date=' + str(datetime.strptime(date_check(args.start_date), "%Y-%m-%d") - timedelta(days=i)).split()[0]
+    c1 = 'from=' + currency_check(args.cf) + '&'
+    c2 = 'to=' + currency_check(args.ct) + '&'
+    number = 'amount=' + str(args.a) + '&'
+    d1 = 'date=' + str(datetime.strptime(date_check(args.save_to_file), "%Y-%m-%d") - timedelta(days=i)).split()[0]
 
     link = link + c1 + c2 + number + d1
 
@@ -43,31 +44,34 @@ def get_site_by_requests(i):
         json.dump(requests.get(link).json(), f, indent=2)
 
 
-def json_to_txt():
-    x = list()
-    y = list()
-
+def json_to_list() -> list:
     with open('exchange_rates.json', 'r') as f:
-        for i in json.load(f).items():
-            y.append(i)
-
-        x.append([y[5][1],
-                  y[2][1]['from'],
-                  y[2][1]['to'],
-                  y[2][1]['amount'],
-                  y[3][1]['rate'],
-                  y[6][1]])
+        content = json.loads(f.read())
+        x = [content['date'],
+             content['query']['from'],
+             content['query']['to'],
+             content['query']['amount'],
+             content['info']['rate'],
+             content['result']]
 
     return x
 
 
 def exchange_rate_writer():
-    y = [['date', 'from', 'to', 'amount', 'rate', 'result']]
+    headers = ['date', 'from', 'to', 'amount', 'rate', 'result']
+    contents = []
+
     for i in range(5):
         get_site_by_requests(i)
-        y.append(json_to_txt())
+        contents.append(json_to_list())
 
-    print(len(y))
+    table = tabulate(contents, headers=headers, tablefmt='pipe', stralign='center')
+
+    if args.save_to_file == 'N':
+        print(table)
+    elif args.save_to_file == 'Y':
+        with open('exchange_rates.txt', 'w') as f:
+            f.write(table)
 
 
 exchange_rate_writer()
