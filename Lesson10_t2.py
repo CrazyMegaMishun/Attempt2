f1f5cae31de3b19a437a7be0843a3b3d4bf53bff
CrazyMegaMishun <62602625+CrazyMegaMishun@users.noreply.github.com>
2022-07-23 21:42:26 +0300

    l10

--- Lesson10_t2.py
+++ Lesson10_t2.py
@@ -2,18 +2,11 @@ import re
 
 
 def check_palindrome(p):
-    p = re.sub('[!? \n-.,]', '', p)
-
-    if len(p) > 1:
-        mid = len(p)//2
-        arr1 = p[:mid].lower()
-        arr2 = p[mid:].lower()
-        arr2 = arr2.replace(arr2[0], '', 1)
-
-        if arr1 == arr2[::-1]:
-            print('Паліндром')
-        else:
-            print('Не паліндром')
+    p = (re.sub('[!? \n-.,—]', '', p).lower())
+    if p.split(sep=p[len(p)//2:])[0] == p.split(sep=p[:len(p)//2])[1].replace(p[len(p)//2], '', 1)[::-1]:
+        print('Базований паліндром')
+    else:
+        print('Не паліндром')
 
 
 check_palindrome(input())
